4013b522759a828d13d3dadd1b75b3043ab21c8b
Mohammed El Haddioui <mel-hadd@c3r9p1.1337.ma>
2025-07-08 21:19:37 +0100

    update

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+mkdir -p /var/www/html
 cd /var/www/html
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
-./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+mv wp-cli.phar /usr/local/bin/wp
+
+wp core download --allow-root
+cp wp-config-sample.php wp-config.php
+
+sed -i "s/define( 'DB_NAME', 'database_name_here' );/define( 'DB_NAME', '${MYSQL_DATABASE}' );/" wp-config.php
+sed -i "s/define( 'DB_USER', 'username_here' );/define( 'DB_USER', '${MYSQL_USER}' );/" wp-config.php
+sed -i "s/define( 'DB_PASSWORD', 'password_here' );/define( 'DB_PASSWORD', '${MYSQL_PASSWORD}' );/" wp-config.php
+sed -i "s/localhost/mariadb/" wp-config.php
 
 php-fpm8.2 -F
